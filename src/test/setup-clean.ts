e6c5af05ed45f6bcacdddb0da64c3e2b0dc1bbfa
Gokul Mallabathula <gokulmallabathula@Gokuls-MacBook-Air.local>
2026-04-01 22:11:36 +0530

    Performed tests

--- src/test/setup-clean.ts
+++ src/test/setup-clean.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "@testing-library/jest-dom";
 import { vi, beforeEach } from 'vitest';
 import { useParkingStore } from '@/lib/parking-store';
